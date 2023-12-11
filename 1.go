d4e180b14ca9eb7b4df0bd344092c4872d02f46b
Lovepreet Singh <lovepreetkaul23@gmail.com>
2023-12-12 01:34:02 +0530

    Solved day 1 part 1

--- 1.go
+++ 1.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"unicode"
+)
 
 func main() {
-	fmt.Println("Hello Advent of code 2023!")
+	var sum int
+
+	f, err := os.Open("1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		sum += calibrate(sc.Text())
+	}
+
+	fmt.Println(sum)
+}
+
+func calibrate(line string) int {
+	var first, last int
+	for _, ch := range line {
+		if unicode.IsDigit(ch) {
+			first, _ = strconv.Atoi(string(ch))
+			break
+		}
+	}
+
+	runeLine := []rune(line)
+	for i := len(line) - 1; i >= 0; i-- {
+		if unicode.IsDigit(runeLine[i]) {
+			last, _ = strconv.Atoi(string(runeLine[i]))
+			break
+		}
+	}
+
+	fmt.Println(first, last)
+	return first*10 + last
 }
